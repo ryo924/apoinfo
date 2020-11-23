360806aa20b05cbde3d5ef28d2aa54196f66e605
ryo924 <ryo.tominaga24@gmail.com>
2020-11-23 19:20:30 +0900

    アポイント詳細機能の実装と表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   get 'appointments/index'
   root to: "appointments#index"
-  resources :appointments, only: [:new, :create]
+  resources :appointments, only: [:new, :create, :show]
 end

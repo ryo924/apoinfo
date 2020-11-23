abb02d59f03f45633cd03d9cbeb861750d1d79d0
ryo924 <ryo.tominaga24@gmail.com>
2020-11-23 19:37:14 +0900

    アポイント編集・削除機能の実装

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -21,6 +21,28 @@ class AppointmentsController < ApplicationController
     @appointment = Appointment.find(params[:id])
   end
 
+  def edit
+    @appointment = Appointment.find(params[:id])
+  end
+
+  def update
+    @appointment = Appointment.find(params[:id])
+    if @appointment.update(appointment_params)
+      redirect_to appointment_path(@appointment)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @appointment = Appointment.find(params[:id])
+    if @appointment.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
   private
   def appointment_params
     params.require(:appointment).permit(:title, :company, :customer, :date, :place, :purpose, :relation_id).merge(user_id: current_user.id)

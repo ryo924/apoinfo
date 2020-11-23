0c44e01d65cc1b7b1b571f7c5e6df75739c6e288
ryo924 <ryo.tominaga24@gmail.com>
2020-11-23 17:09:55 +0900

    アソシエーションとバリデーションの設定

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,2 +1,15 @@
 class Appointment < ApplicationRecord
+
+  with_options presence: true do
+    validates :title
+    validates :company
+    validates :customer
+    validates :date
+    validates :place
+    validates :purpose
+    validates :relation_id
+  end
+
+  belongs_to :user
+
 end
